a5118e713c6f235f5b74c3676e2db49a4f629a44
김주연 <jyeon3930@naver.com>
2022-11-09 05:26:01 +0900

    [FE] feat: 마크다운 에디터 이미지 hook 기능 구현

--- frontend/src/features/post/PostImageTest.tsx
+++ frontend/src/features/post/PostImageTest.tsx
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { Editor } from "@toast-ui/react-editor";
+import "@toast-ui/editor/dist/toastui-editor.css";
 import axios from "api/MultipartAxios";
 import api from "api/Api";
 
 const PostImageTest = () => {
   const [showImages, setShowImages] = useState([]);
   const [fileList, setFileList] = useState([]);
+
+  const editorRef = useRef<Editor>();
 
   const handleAddImages = (e: React.ChangeEvent<HTMLInputElement>) => {
     const uploadFile = Array.from(e.target.files);
@@ -14,7 +18,7 @@ const PostImageTest = () => {
     for (let i = 0; i < uploadFile.length; i++) {
       const currentImageUrl = URL.createObjectURL(uploadFile[i]);
       imageUrlLists.push(currentImageUrl);
-      uploadFileLists.push(uploadFile);
+      uploadFileLists.push(uploadFile[i]);
     }
 
     setShowImages(imageUrlLists);
@@ -38,7 +42,7 @@ const PostImageTest = () => {
     const writeDetailPostRequest = {
       accessToken: "GwezHgZHEC-rVvDAJiAGCP2udbXgnH4-1lvv3AYKkvO3KxFSEt1-fTAqTPJQuukl",
       githubId: "juuyeon",
-      content: "## TEST\n ![images](lion.png)",
+      content: "## TEST\n ![images](lion_profile.png)",
     };
 
     formData.append(
@@ -56,6 +60,11 @@ const PostImageTest = () => {
       });
   };
 
+  // useEffect(() => {
+  //   // 이미지 드래그 업로드 막기
+  //   editorRef.current.getInstance().removeHook("addImageBlobHook");
+  // }, []);
+
   return (
     <div>
       <label htmlFor="input-file">
@@ -70,6 +79,34 @@ const PostImageTest = () => {
       ))}
 
       <button onClick={testModify}>글 수정하기</button>
+
+      <Editor
+        ref={editorRef}
+        initialValue=""
+        previewStyle="vertical"
+        height="70vh"
+        initialEditType="markdown"
+        hooks={{
+          addImageBlobHook: (blob, callback) => {
+            const uploadFileLists = [...fileList];
+            const imageUrlLists = [...showImages];
+
+            const currentImageUrl = URL.createObjectURL(blob);
+            imageUrlLists.push(currentImageUrl);
+            uploadFileLists.push(blob);
+
+            setShowImages(imageUrlLists);
+            setFileList(uploadFileLists);
+            console.log(imageUrlLists);
+            console.log(uploadFileLists);
+            console.log(fileList);
+
+            callback(currentImageUrl);
+
+            return false;
+          },
+        }}
+      ></Editor>
     </div>
   );
 };
